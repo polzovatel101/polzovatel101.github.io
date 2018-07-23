0c0f2cd7b43f5928025fd714eed276851c1e131c
Ilia Yanovoy <polzovatel101@mail.ru>
2018-07-23 21:58:53 +0300

    create tested site v2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('');
+document.addEventListener('DOMContentLoaded', function () {
+  let parentBlock = $('.flexbox__parent');
+  let childBlock = $('.selected-box');
+  let alignItems = $('[name]');
+
+  alignItems.click((event) => {
+    const valueOfProperty = event.currentTarget.id.split(' ')[1]
+      ? event.currentTarget.id.split(' ')[1]
+      : event.currentTarget.id.split(' ')[0];
+    if (event.currentTarget.className != 'for-child') {
+      parentBlock.css(event.currentTarget.name, valueOfProperty);
+    } else {
+      childBlock.css(event.currentTarget.name, valueOfProperty);
+    }
+  });
 });
